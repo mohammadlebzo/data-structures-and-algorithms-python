dc1401a4d4a45cb89109b103c502154de3eeec46
unknown <mohlebzo1@gmail.com>
2022-06-29 22:13:10 +0300

    Finished the coding challenge, and added it to the table of content

---
+++ stack_queue_brackets/tests/test_stack_queue_brackets.py
@@ -0,0 +1,19 @@
+import pytest
+from stack_queue_brackets.stack_queue_brackets import validate_brackets
+
+@pytest.mark.parametrize(
+    "test_string,expected",
+    [
+        ("{}", True),
+        ("{}(){}", True),
+        ("()[[Extra Characters]]", True),
+        ("(){}[[]]", True),
+        ("{}{Code}[Fellows](())", True),
+        ("[({}]", False),
+        ("(](", False),
+        ("{(})", False),
+    ],
+)
+def test_all_cases(test_string, expected):
+    actual = validate_brackets(test_string)
+    assert actual == expected
